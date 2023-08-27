634327343822e2cfcd478277904c9c0532b5595d
shygyver <shygyver@outlook.com>
2023-08-27 17:57:46 +0200

    comments

--- src/routes/cors-options.ts
+++ src/routes/cors-options.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 import routing from '@novice1/routing';
 
-// enable CORS (Cross-origin resource sharing) options request
+// enable CORS (Cross-origin resource sharing) pre-flight request
 export default routing().options({
     path: '*'
 }, cors());
